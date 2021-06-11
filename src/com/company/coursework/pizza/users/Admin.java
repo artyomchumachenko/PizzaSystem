598e7806dba3afd51cd609d5202734a9b49d9032
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-06-11 19:02:43 +0300

    Initial commit

---
+++ src/com/company/coursework/pizza/users/Admin.java
@@ -0,0 +1,7 @@
+package com.company.coursework.pizza.users;
+
+public class Admin extends User {
+    public Admin(String nickname, String login, String password) {
+        super(nickname, login, password);
+    }
+}
